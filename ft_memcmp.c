3bb0a9e726d95ff6fdda62227c434063c9c923f2
Riescent <91223304+Riescent@users.noreply.github.com>
2022-10-09 18:07:58 +0800

    Improved readability of ft_memsmp()

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/09 14:35:17 by vfries            #+#    #+#             */
-/*   Updated: 2022/10/09 17:53:31 by vfries           ###   ########lyon.fr   */
+/*   Updated: 2022/10/09 18:07:16 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	while (n > 0)
 	{
-		if (*(char *)s1 < *(char *)s2 || *(char *)s1 > *(char *)s2)
+		if (*(char *)s1 < *(char *)s2
+			|| *(char *)s1 > *(char *)s2)
 			return (*(char *)s1 - *(char *)s2);
 		s1++;
 		s2++;
