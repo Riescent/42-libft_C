6d622074f84a33560d6c4b2e5cefcd7dcdfa5655
Riescent <91223304+Riescent@users.noreply.github.com>
2022-10-09 14:44:55 +0800

    refactored ft_strncmp()

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,31 +6,25 @@
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 18:53:28 by vfries            #+#    #+#             */
-/*   Updated: 2022/10/09 13:05:16 by vfries           ###   ########lyon.fr   */
+/*   Updated: 2022/10/09 14:42:53 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strncmp(const char *s1, const char *s2, unsigned int n)
 {
-	unsigned int	i_1;
-	unsigned int	i_2;
-
-	if (n == 0)
-		return (0);
-	i_1 = 0;
-	i_2 = 0;
-	while (i_1 < n && i_2 < n)
+	while (n > 0)
 	{
-		if (s1[i_1] < s2[i_2])
+		if (*s1 < *s2)
 			return (-1);
-		if (s1[i_1] > s2[i_2])
+		if (*s1 > *s2)
 			return (1);
-		if (!(s1[i_1] + s2[i_2]))
+		if (!(*s1 + *s2))
 			return (0);
-		if (s1[i_1])
-			i_1++;
-		if (s2[i_2])
-			i_2++;
+		if (*s1)
+			s1++;
+		if (*s2)
+			s2++;
+		n--;
 	}
 	return (0);
 }
