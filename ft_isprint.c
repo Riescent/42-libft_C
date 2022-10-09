36a6995a80c2bcab8ac3302074a8c659a8007db9
Riescent <91223304+Riescent@users.noreply.github.com>
2022-10-09 18:05:12 +0800

    Improved readability of ft_isprint()

--- ft_isprint.c
+++ ft_isprint.c
@@ -6,13 +6,14 @@
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 17:59:02 by vfries            #+#    #+#             */
-/*   Updated: 2022/10/03 18:19:38 by vfries           ###   ########lyon.fr   */
+/*   Updated: 2022/10/09 18:04:10 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+// See ASCII table
 int	ft_isprint(int c)
 {
-	if (c < 32 || c >= 127)
+	if (c < ' ' || c > '~')
 		return (0);
 	return (1);
 }
