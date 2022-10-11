470ba5258253bbd886830880d4f82788a8dfe590
Riescent <91223304+Riescent@users.noreply.github.com>
2022-10-11 16:48:09 +0800

    ft_calloc() now sets errno to ENOMEM when malloc fails

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,11 +6,12 @@
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 14:09:07 by vfries            #+#    #+#             */
-/*   Updated: 2022/10/11 16:42:02 by vfries           ###   ########lyon.fr   */
+/*   Updated: 2022/10/11 16:46:38 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <sys/errno.h>
 
 void	*ft_calloc(size_t count, size_t size)
 {
@@ -21,7 +22,10 @@ void	*ft_calloc(size_t count, size_t size)
 	total_size = count * size;
 	result = malloc(total_size);
 	if (result == NULL)
+	{
+		errno = ENOMEM;
 		return (NULL);
+	}
 	i = 0;
 	while (i < total_size)
 		result[i++] = 0;
