94ddadf88e981cbb2ffec18d7116d75b481d1e55
Riescent <91223304+Riescent@users.noreply.github.com>
2022-10-13 22:50:58 +0800

    refactored ft_putnbr_fd()

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 20:44:25 by vfries            #+#    #+#             */
-/*   Updated: 2022/10/12 20:59:30 by vfries           ###   ########lyon.fr   */
+/*   Updated: 2022/10/13 22:50:32 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,18 +25,16 @@ static void	ft_putnbr_fd_recursion(long n, int fd)
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	long	long_n;
-
 	if (n == 0)
 	{
 		write(fd, "0", 1);
 		return ;
 	}
-	long_n = n;
-	if (long_n < 0)
+	if (n < 0)
 	{
 		write(fd, "-", 1);
-		long_n = -long_n;
+		ft_putnbr_fd_recursion(-(long)n, fd);
 	}
-	ft_putnbr_fd_recursion(long_n, fd);
+	else
+		ft_putnbr_fd_recursion(n, fd);
 }
