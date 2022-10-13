442ed9911f4a1993d1489cb7b8f24e94fffb8ca9
Riescent <91223304+Riescent@users.noreply.github.com>
2022-10-13 22:23:02 +0800

    update ft_itoa()

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,35 +6,35 @@
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 18:18:37 by vfries            #+#    #+#             */
-/*   Updated: 2022/10/12 18:50:47 by vfries           ###   ########lyon.fr   */
+/*   Updated: 2022/10/13 22:22:23 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
 
 char	*ft_itoa(int n)
 {
 	char	buf[12];
 	char	*buf_ptr;
 	char	is_negative;
-	long	long_n;
 
 	if (n == 0)
 		return (ft_strdup("0"));
-	long_n = n;
-	is_negative = 0;
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
 	if (n < 0)
 	{
-		long_n = -long_n;
+		n = -n;
 		is_negative = 1;
 	}
+	else
+		is_negative = 0;
 	buf[11] = '\0';
 	buf_ptr = buf + 10;
-	while (long_n > 0)
+	while (n > 0)
 	{
-		*buf_ptr-- = long_n % 10 + '0';
-		long_n /= 10;
+		*buf_ptr-- = n % 10 + '0';
+		n /= 10;
 	}
 	if (is_negative)
 		*buf_ptr-- = '-';
