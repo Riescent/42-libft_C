95c2fb8ab800a2668cebf619d5676eaa06f545fd
Riescent <91223304+Riescent@users.noreply.github.com>
2022-10-13 16:04:20 +0800

    ft_lstiter()

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/09/07 19:13:43 by vfries            #+#    #+#              #
-#    Updated: 2022/10/13 15:50:59 by vfries           ###   ########lyon.fr    #
+#    Updated: 2022/10/13 16:02:42 by vfries           ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
@@ -52,7 +52,8 @@ SRCS_BONUS =	ft_lstnew_bonus.c		\
 				ft_lstlast_bonus.c		\
 				ft_lstadd_back_bonus.c	\
 				ft_lstdelone_bonus.c	\
-				ft_lstclear_bonus.c
+				ft_lstclear_bonus.c		\
+				ft_lstiter_bonus.c
 OBJS_BONUS =	${SRCS_BONUS:.c=.o}
 FLAG =			-Wall -Wextra -Werror
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 19:22:16 by vfries            #+#    #+#             */
-/*   Updated: 2022/10/13 15:51:25 by vfries           ###   ########lyon.fr   */
+/*   Updated: 2022/10/13 16:02:52 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,5 +66,6 @@ t_list				*ft_lstlast(t_list *lst);
 void				ft_lstadd_back(t_list **lst, t_list *new);
 void				ft_lstdelone(t_list *lst, void (*del)(void *));
 void				ft_lstclear(t_list **lst, void (*del)(void *));
+void				ft_lstiter(t_list *lst, void (*f)(void *));
 
 #endif
