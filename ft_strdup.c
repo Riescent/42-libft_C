2200a9a024b0b30f646ab48740fc2ed21bea424c
Riescent <91223304+Riescent@users.noreply.github.com>
2022-10-13 22:33:05 +0800

    ft_strdup() removed errno = ENOMEM; since it is already done in malloc()

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,13 +6,12 @@
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 16:43:45 by vfries            #+#    #+#             */
-/*   Updated: 2022/10/11 17:03:09 by vfries           ###   ########lyon.fr   */
+/*   Updated: 2022/10/13 22:32:06 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
-#include <sys/errno.h>
 
 char	*ft_strdup(const char *s1)
 {
@@ -21,10 +20,7 @@ char	*ft_strdup(const char *s1)
 
 	new_str = malloc(sizeof(char) * (ft_strlen(s1) + 1));
 	if (new_str == NULL)
-	{
-		errno = ENOMEM;
 		return (NULL);
-	}
 	new_str_start = new_str;
 	while (*s1)
 		*new_str++ = *s1++;
