a8af5d41a68f20b420133dc85cb5d03cfb45ddce
Riescent <91223304+Riescent@users.noreply.github.com>
2022-10-13 22:01:08 +0800

    ft_isprint() removed useless comment

--- ft_isprint.c
+++ ft_isprint.c
@@ -6,11 +6,10 @@
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 17:59:02 by vfries            #+#    #+#             */
-/*   Updated: 2022/10/09 18:04:10 by vfries           ###   ########lyon.fr   */
+/*   Updated: 2022/10/13 22:00:35 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-// See ASCII table
 int	ft_isprint(int c)
 {
 	if (c < ' ' || c > '~')
