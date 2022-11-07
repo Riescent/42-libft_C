eb2ecdb0a1a9eaa4fc6eac1a191cad4bbd6d39c6
vfries <vfries@student.42lyon.fr>
2022-11-07 19:42:18 +0100

    Refactored ft_substr()

--- ft_substr.c
+++ ft_substr.c
@@ -6,28 +6,25 @@
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 17:07:00 by vfries            #+#    #+#             */
-/*   Updated: 2022/11/07 14:49:12 by vfries           ###   ########lyon.fr   */
+/*   Updated: 2022/11/07 19:40:16 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include "libft.h"
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*result;
-	size_t	result_len;
-	char	*result_start;
+	size_t	result_size;
 
 	s += start;
-	result_len = 0;
-	while (s[result_len] && result_len < len)
-		result_len++;
-	result = malloc(sizeof(char) * result_len + 1);
+	result_size = 0;
+	while (s[result_size] && result_size < len)
+		result_size++;
+	result = malloc(sizeof(char) * ++result_size);
 	if (result == NULL)
 		return (NULL);
-	result_start = result;
-	while (result_len--)
-		*result++ = *s++;
-	*result = '\0';
-	return (result_start);
+	ft_strlcpy(result, s, result_size);
+	return (result);
 }
