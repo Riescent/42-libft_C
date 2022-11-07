6ad91dc9038dc048cbf47298778579cd1e3fcdc8
vfries <vfries@student.42lyon.fr>
2022-11-07 14:36:44 +0100

    Refactored ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/09 14:35:17 by vfries            #+#    #+#             */
-/*   Updated: 2022/10/09 21:54:36 by vfries           ###   ########lyon.fr   */
+/*   Updated: 2022/11/07 14:35:28 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	while (n > 0)
 	{
-		if (*(unsigned char *)s1 < *(unsigned char *)s2
-			|| *(unsigned char *)s1 > *(unsigned char *)s2)
+		if (*(unsigned char *)s1 != *(unsigned char *)s2)
 			return (*(unsigned char *)s1 - *(unsigned char *)s2);
 		s1++;
 		s2++;
