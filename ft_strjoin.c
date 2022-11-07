7ba570962cdfa43cab5be05e8f0cfbad73e18b6c
vfries <vfries@student.42lyon.fr>
2022-11-07 18:15:21 +0100

    Secured malloc() in ft_strjoin()

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 17:21:43 by vfries            #+#    #+#             */
-/*   Updated: 2022/10/11 17:28:51 by vfries           ###   ########lyon.fr   */
+/*   Updated: 2022/11/07 18:13:12 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	result_size = ft_strlen(s1) + ft_strlen(s2) + 1;
 	result = malloc(result_size);
+	if (result == NULL)
+		return (NULL);
 	ft_strlcpy(result, s1, result_size);
 	ft_strlcat(result, s2, result_size);
 	return (result);
