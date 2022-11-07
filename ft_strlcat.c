18edecb66f88554373803aa69dcea2c8b3b266b8
vfries <vfries@student.42lyon.fr>
2022-11-07 14:05:26 +0100

    Renamed variable in ft_strlcat()

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,19 +6,19 @@
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 21:39:19 by vfries            #+#    #+#             */
-/*   Updated: 2022/10/11 13:49:57 by vfries           ###   ########lyon.fr   */
+/*   Updated: 2022/11/07 14:00:08 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t size)
+size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
 {
 	size_t	dst_len;
 
 	dst_len = ft_strlen(dst);
-	if (dst_len >= size)
-		return (size + ft_strlen(src));
-	return (dst_len + ft_strlcpy(dst + dst_len, src, size - dst_len));
+	if (dst_len >= dstsize)
+		return (dstsize + ft_strlen(src));
+	return (dst_len + ft_strlcpy(dst + dst_len, src, dstsize - dst_len));
 }
