64d04b3cad5ec1fd2f4dbc5d2c3a654819c3b55e
vfries <vfries@student.42lyon.fr>
2022-11-07 15:11:53 +0100

    Refactored ft_putnbr_fd()

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 20:44:25 by vfries            #+#    #+#             */
-/*   Updated: 2022/10/13 22:50:32 by vfries           ###   ########lyon.fr   */
+/*   Updated: 2022/11/07 15:10:05 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,8 @@ static void	ft_putnbr_fd_recursion(long n, int fd)
 void	ft_putnbr_fd(int n, int fd)
 {
 	if (n == 0)
-	{
 		write(fd, "0", 1);
-		return ;
-	}
-	if (n < 0)
+	else if (n < 0)
 	{
 		write(fd, "-", 1);
 		ft_putnbr_fd_recursion(-(long)n, fd);
