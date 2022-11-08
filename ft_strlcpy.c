de0b207199bc361dc0710e8e31801719e034857a
vfries <vfries@student.42lyon.fr>
2022-11-08 19:43:07 +0100

    Fixes

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,16 +6,19 @@
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 13:36:26 by vfries            #+#    #+#             */
-/*   Updated: 2022/11/07 23:08:44 by vfries           ###   ########lyon.fr   */
+/*   Updated: 2022/11/08 18:58:37 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include <stdlib.h>
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	src_len;
 
+	if (size == 0)
+		return (ft_strlen(src));
 	src_len = 0;
 	while (*src)
 	{
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 18:53:28 by vfries            #+#    #+#             */
-/*   Updated: 2022/11/07 19:34:31 by vfries           ###   ########lyon.fr   */
+/*   Updated: 2022/11/08 18:59:48 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,10 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	while (n-- > 0)
+	while (n-- > 0 && *(unsigned char *)s1 + *(unsigned char *)s2 != 0)
 	{
-		if (*s1 != *s2)
-			return (*s1 - *s2);
-		if (*s1 + *s2 == 0)
-			return (0);
+		if (*(unsigned char *)s1 != *(unsigned char *)s2)
+			return (*(unsigned char *)s1 - *(unsigned char *)s2);
 		s1++;
 		s2++;
 	}
