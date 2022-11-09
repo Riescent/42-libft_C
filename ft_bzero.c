81562fdaa61c56e4ecd73b77b79d5e9675c9ab46
vfries <vfries@student.42lyon.fr>
2022-11-09 23:41:01 +0100

    Refactoring

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,14 +6,14 @@
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/09 15:59:28 by vfries            #+#    #+#             */
-/*   Updated: 2022/11/07 18:18:56 by vfries           ###   ########lyon.fr   */
+/*   Updated: 2022/11/09 23:32:00 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include <stdlib.h>
 
 void	ft_bzero(void *s, size_t n)
 {
-	while (n-- > 0)
-		*(unsigned char *)s++ = 0;
+	ft_memset(s, 0, n);
 }
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,10 +6,11 @@
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 15:11:33 by vfries            #+#    #+#             */
-/*   Updated: 2022/11/09 00:12:40 by vfries           ###   ########lyon.fr   */
+/*   Updated: 2022/11/09 23:39:46 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include <stdlib.h>
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
@@ -18,8 +19,7 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 
 	start_dst = dst;
 	if (dst > src)
-		while (len--)
-			((unsigned char *)dst)[len] = ((unsigned char *)src)[len];
+		ft_memcpy(dst, src, len);
 	else if (dst < src)
 		while (len--)
 			*(unsigned char *)dst++ = *(unsigned char *)src++;
