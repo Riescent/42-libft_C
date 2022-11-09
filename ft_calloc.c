f751a8b6b29b480ee2a34a7bcdd53f4bcd704c9b
vfries <vfries@student.42lyon.fr>
2022-11-09 22:35:02 +0100

    Fixed calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 14:09:07 by vfries            #+#    #+#             */
-/*   Updated: 2022/11/08 15:05:48 by vfries           ###   ########lyon.fr   */
+/*   Updated: 2022/11/09 21:40:47 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,7 @@ void	*ft_calloc(size_t count, size_t size)
 	size_t			total_size;
 
 	if (count == 0 || size == 0)
-	{
-		count = 1;
-		size = 1;
-	}
+		return (malloc(0));
 	else if (SIZE_MAX / count < size)
 		return (NULL);
 	total_size = count * size;
