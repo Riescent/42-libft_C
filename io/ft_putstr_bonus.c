efa2185c4d6eb88e51839a58b334dc4074678bfe
vfries <vfries@student.42lyon.fr>
2022-11-20 08:10:46 +0100

    Fixed ft_putstr()

--- io/ft_putstr_bonus.c
+++ io/ft_putstr_bonus.c
@@ -1,19 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*   ft_putstr_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 12:22:54 by vfries            #+#    #+#             */
-/*   Updated: 2022/11/19 12:24:26 by vfries           ###   ########lyon.fr   */
+/*   Updated: 2022/11/20 08:09:31 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_string.h"
 #include <unistd.h>
 
-void	ft_putstr_fd(char *s)
+void	ft_putstr(char *s)
 {
 	if (s == NULL)
 		return ;
