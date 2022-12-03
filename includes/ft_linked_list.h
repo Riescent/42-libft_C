f9ec3df263a5d5ec11ff0ee759b4445b2a99b39f
vfries <vfries@student.42lyon.fr>
2022-12-03 02:48:42 +0100

    Fixed ft_lst_get_next_free_current()

--- includes/ft_linked_list.h
+++ includes/ft_linked_list.h
@@ -6,7 +6,7 @@
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 11:01:01 by vfries            #+#    #+#             */
-/*   Updated: 2022/12/03 02:32:51 by vfries           ###   ########lyon.fr   */
+/*   Updated: 2022/12/03 02:48:01 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,18 @@ typedef struct s_list
 {
 	void			*content;
 	struct s_list	*next;
-}					t_list;
+}	t_list;
 
-t_list				*ft_lst_get_next_free_current(t_list *lst);
-void				ft_lst_reverse(t_list **lst);
-void				ft_lstadd_back(t_list **lst, t_list *new);
-void				ft_lstadd_front(t_list **lst, t_list *new);
-void				ft_lstclear(t_list **lst, void (*del)(void *));
-void				ft_lstdelone(t_list *lst, void (*del)(void *));
-void				ft_lstiter(t_list *lst, void (*f)(void *));
-t_list				*ft_lstlast(t_list *lst);
-t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
-						void (*del)(void *));
-t_list				*ft_lstnew(void *content);
-int					ft_lstsize(t_list *lst);
+t_list	*ft_lst_get_next_free_current(t_list *lst, void (*del)(void *));
+void	ft_lst_reverse(t_list **lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstlast(t_list *lst);
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list	*ft_lstnew(void *content);
+int		ft_lstsize(t_list *lst);
 
 #endif
--- srcs/linked_list/ft_lst_get_next_free_current.c
+++ srcs/linked_list/ft_lst_get_next_free_current.c
@@ -6,20 +6,21 @@
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 02:30:21 by vfries            #+#    #+#             */
-/*   Updated: 2022/12/03 02:33:10 by vfries           ###   ########lyon.fr   */
+/*   Updated: 2022/12/03 02:46:47 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_linked_list.h"
 #include <stdlib.h>
 
-t_list	*ft_lst_get_next_free_current(t_list *lst)
+t_list	*ft_lst_get_next_free_current(t_list *lst, void (*del)(void *))
 {
 	t_list	*next;
 
 	if (lst == NULL)
 		return (NULL);
 	next = lst->next;
+	del(lst->content);
 	free(lst);
 	return (next);
 }
