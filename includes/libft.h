ee8137cc46812c5d9fb2d9b6f97c8889f058853b
Riescent <91223304+Riescent@users.noreply.github.com>
2022-12-18 15:13:12 +0100

    Updated libft.h

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 19:22:16 by vfries            #+#    #+#             */
-/*   Updated: 2022/11/19 12:28:59 by vfries           ###   ########lyon.fr   */
+/*   Updated: 2022/12/18 15:12:22 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 # define LIBFT_H
 
 # include "ft_char.h"
+# include "ft_get_next_line.h"
 # include "ft_io.h"
 # include "ft_linked_list.h"
+# include "ft_math.h"
 # include "ft_mem.h"
+# include "ft_numbers.h"
 # include "ft_string.h"
-
-char	*ft_itoa(int n);
 
 #endif
