0bd4b5b8e3bfbe524535c644dc3dac92ad92ca03
Riescent <91223304+Riescent@users.noreply.github.com>
2023-01-06 06:58:49 +0100

    Removed useless comment

--- srcs/linked_list/int/lsti_cmp.c
+++ srcs/linked_list/int/lsti_cmp.c
@@ -6,16 +6,11 @@
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 03:24:48 by vfries            #+#    #+#             */
-/*   Updated: 2023/01/06 06:50:34 by vfries           ###   ########lyon.fr   */
+/*   Updated: 2023/01/06 06:58:14 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_linked_list.h"
-
-// ft_lsti_cmp() doesn't do:
-// if (lst_1->content != lst_2->content)
-//			return (lst_1->content - lst_2->content);
-// Because I'm scared of possible overflow
 
 int	ft_lsti_cmp(t_list_i *lst_1, t_list_i *lst_2)
 {
